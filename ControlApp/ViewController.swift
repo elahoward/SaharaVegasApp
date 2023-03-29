51a1baf4a4961a60e2867fe4efba16143ac0813d
ehoward <ehoward@tfs.ca>
2023-03-29 14:30:05 -0400

    Initial Commit

---
+++ ControlApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ControlApp
+//
+//  Created by Ela Howard on 2023-03-29.
+//  Copyright © 2023 Ela Howard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
